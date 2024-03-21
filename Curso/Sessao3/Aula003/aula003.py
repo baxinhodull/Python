c6facba996cbd97c3e2aacb2df8527b236f4bd06
Carlos Henrique <93613216+baxinhodull@users.noreply.github.com>
2024-03-21 11:59:14 -0300

    Delete Curso directory

--- Curso/Sessao3/Aula003/aula003.py
+++
@@ -1,4 +0,0 @@
-if 5 > 20:
-    print("Cinco é maior do que dois")
-    print("Fim do bloco do if.")
-print("Fora do bloco if.")
--- Curso/Sessao6/aula014.py
+++
@@ -1,8 +0,0 @@
-nome = "Gabriel Artigas"
-print(nome[8:12])
-print(nome[:8])
-print(nome[8:])
-print(nome[-5:-2])
-
-fatiaNome = nome[3:9]
-print(fatiaNome)
